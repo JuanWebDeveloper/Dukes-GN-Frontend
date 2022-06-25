2208b5c0b994ee827297c0e299f33b57ab85ab84
Juan Vargas <juandeveloper19@gmail.com>
2022-06-25 18:54:03 -0500

    [FEAT] Mejoras en el footer
    Se mejoro los estilos para el footer
    
    HU-Componentes compartidos.

--- src/app/routes/private/private-routing.module.ts
+++ src/app/routes/private/private-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LayoutComponent } from '../../ui/layout/layout.component';
-import { NavbarComponent } from 'src/app/ui/shared/navbar/navbar.component';
-import { FooterComponent } from 'src/app/ui/shared/footer/footer.component';
-import { SigninComponent } from 'src/app/ui/components/signin/signin.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      { path: '', component: NavbarComponent, outlet: 'navbar' },
-      { path: '', component: SigninComponent },
-      { path: '', component: FooterComponent, outlet: 'footer' },
-    ],
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
