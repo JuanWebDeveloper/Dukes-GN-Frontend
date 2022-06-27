0b4de821ebf6d449b67377e7ac93c6399b1a565f
DMosqueral <dml2410@gmail.com>
2022-06-27 12:19:47 -0500

    [FIX] Corrección Texto de las  Notificaciones
    Se mejora la redacción de los mensajes de las ventanas Pop Ups (Toast) para una  mejor experiencia de usuario
    
    HU - Notificación de errores

--- src/app/core/utils/messages.util.ts
+++ src/app/core/utils/messages.util.ts
@@ -4,22 +4,22 @@ export class MessagesUtil {
         switch (code) {
             //El correo ya existe
             case 'auth/email-already-in-use':
-                return "El correo ya existe";
+                return "El correo ya se encuentra registrado.";
             // Contraseña debil
             case 'auth/weak-password':
-                return "La contraseña es muy debil";
+                return "La contraseña no cumple con los requisitos mínimos de seguridad.";
             // Correo inválido
             case 'auth/invalid-email':
-                return "Correo inválido";
+                return "Correo inválido. Verifique el correo.";
             // Contraseña incorrecta
             case 'auth/wrong-password':
-                return "Contraseña incorrecta"
+                return "Contraseña incorrecta. Intente nuevamente."
             // El usuario no existe
             case 'auth/user-not-found':
-                return "El correo no existe"
+                return "El correo no existe. Por favor registrese."
             // Muchas peticiones de acceso denegados
             case 'auth/too-many-requests':
-                return "Correo desabilitado por varios intentos de ingreso"
+                return "Acceso Denegado. Superaste la cantidad máxima de ingresos. Solicita recuperación de contraseña."
             default:
                 return "Error desconocido";
         }
