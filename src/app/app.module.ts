e64f664babe1327a0fe8efce1a418abc93149162
Juan Vargas <juandeveloper19@gmail.com>
2022-06-27 03:38:23 -0500

    [FEAT] Configuracion de firebase
    Se inicializo firebase en la aplicacion para poder usar sus servicios
    
    HU-Registo de usuarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
 
 // Routings.
 import { AppRoutingModule } from './app-routing.module';
+
+// Environment variables
+import { environment } from '../environments/environment';
 
 // Components.
 import { AppComponent } from './app.component';
@@ -9,7 +13,11 @@ import { ComponentsModule } from './ui/components.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [ComponentsModule, AppRoutingModule],
+  imports: [
+    ComponentsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDmi1G2K7mLdmaNnD_RNbPHEz5CbU5mZAA',
+    authDomain: 'dukes-9be6f.firebaseapp.com',
+    projectId: 'dukes-9be6f',
+    storageBucket: 'dukes-9be6f.appspot.com',
+    messagingSenderId: '976937121962',
+    appId: '1:976937121962:web:4cd99331b95b33f1adf8d3',
+    measurementId: 'G-HZGHT38MH2',
+  },
 };
