e15b1c94e4cee60c88c9f3454cbb978751765dee
Juan Vargas <juandeveloper19@gmail.com>
2022-06-28 17:01:26 -0500

    [FEAT] Loading
    Se creo el loading de la aplicacion

--- src/app/ui/tabs/list-user/list-user.component.ts
+++ src/app/ui/tabs/list-user/list-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { UserService } from 'src/app/core/services/user.service';
 import { User } from 'src/app/core/models/User';
@@ -8,15 +8,22 @@ import { User } from 'src/app/core/models/User';
   templateUrl: './list-user.component.html',
   styleUrls: ['./list-user.component.scss'],
 })
-export class ListUserComponent implements OnInit {
+export class ListUserComponent implements OnInit, OnDestroy {
   public users: User[] | undefined;
+  public loading: boolean = true;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe((users: User[]) => {
       this.users = users;
+      this.loading = false;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.users = undefined;
+    this.loading = true;
   }
 
   public showIcon(state: boolean) {
