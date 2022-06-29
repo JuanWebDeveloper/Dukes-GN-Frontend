8d6317b55e31dbff5a34dbb3bf2171057da2c7f8
Juan Vargas <juandeveloper19@gmail.com>
2022-06-28 22:53:30 -0500

    [FEAT] Formulario para crear programas
    Se creo el formulario para crear los programas en el panel de los admin
    
    HU-Interfaz para el admin

--- src/app/ui/tabs/create-program/create-program.component.ts
+++ src/app/ui/tabs/create-program/create-program.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'dukes-create-program',
   templateUrl: './create-program.component.html',
-  styleUrls: ['./create-program.component.scss']
+  styleUrls: ['./create-program.component.scss'],
 })
 export class CreateProgramComponent implements OnInit {
+  public courses: any[] = [
+    {
+      courseName: 'course' + 1,
+    },
+    {
+      courseName: 'course' + 3,
+    },
+    {
+      courseName: 'course' + 4,
+    },
+  ];
+  public modules: any[] = [
+    {
+      moduleName: 'module' + 1,
+    },
+    {
+      moduleName: 'module' + 2,
+    },
+    {
+      moduleName: 'module' + 3,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public addCourse(): void {
+    this.courses.push({
+      courseName: 'course' + this.courses.length,
+    });
   }
 
+  public addModule(): void {
+    this.modules.push({
+      moduleName: 'module' + this.modules.length,
+    });
+  }
+
+  // Crear programas.
+  public async onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
 }
