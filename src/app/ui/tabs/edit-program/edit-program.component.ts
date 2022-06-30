4043e7a2ceb32d983e1eac16143a14d03cc267b4
Juan Vargas <juandeveloper19@gmail.com>
2022-06-30 07:33:46 -0500

    [FEAT] Mostrando informacion
    Mostrando la informacion del programa en el componente de editar programa
    
    HU-Edicion de programa.

--- src/app/ui/tabs/edit-program/edit-program.component.ts
+++ src/app/ui/tabs/edit-program/edit-program.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+/**
+ * Servicios para actualizar los datos de los programas.
+ **/
+import { Program } from 'src/app/core/models/Program';
+import { Course } from 'src/app/core/models/Course';
+import { Module } from 'src/app/core/models/Module';
 
 @Component({
   selector: 'dukes-edit-program',
   templateUrl: './edit-program.component.html',
-  styleUrls: ['./edit-program.component.scss']
+  styleUrls: ['./edit-program.component.scss'],
 })
 export class EditProgramComponent implements OnInit {
+  @Input() program: Program | any;
+  @Input() courses: Course[] | any;
+  @Input() modules: Module[] | any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
