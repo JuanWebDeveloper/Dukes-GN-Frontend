70ddf30e701a751950d3db714eccd996dc0f5be0
juantoro7295 <juanpatoro2011@gmail.com>
2022-07-01 04:38:37 -0500

    [FEAT] Validacion Componente: Pogress
    Se valida que se encuentre un componente creado en el progregclear

--- src/app/ui/tabs/create-program/create-program.component.ts
+++ src/app/ui/tabs/create-program/create-program.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 /**
