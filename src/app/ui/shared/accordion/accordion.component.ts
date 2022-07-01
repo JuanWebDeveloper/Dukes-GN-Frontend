d53aedc6bb06e18105ec2ca62864cf34b06ccd21
Juan Vargas <juandeveloper19@gmail.com>
2022-07-01 07:18:17 -0500

    [FEAT] Filtrado de usuarios
    Se organizo el formulario que asigna las notas
    
    HU-Asignación de notas.

--- src/app/ui/shared/accordion/accordion.component.ts
+++ src/app/ui/shared/accordion/accordion.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { UserService } from '../../../core/services/user.service';
 import { Program } from '../../../core/models/Program';
 import { Course } from 'src/app/core/models/Course';
 import { Module } from 'src/app/core/models/Module';
+import { User } from '../../../core/models/User';
 
 @Component({
   selector: 'dukes-accordion',
@@ -12,13 +14,20 @@ import { Module } from 'src/app/core/models/Module';
 })
 export class AccordionComponent implements OnInit {
   @Input() program: Program | undefined;
-  @Input() courses: Course[] | any;
-  @Input() modules: Module[] | any;
+  @Input() courses: Course[] | undefined;
+  @Input() modules: Module[] | undefined;
   @Input() userId: string | undefined;
+  public programUsers: User[] | any;
 
   constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe((users: User[]) => {
+      this.programUsers = users.filter(
+        (user: User) => user.programId === this.program!.id_program
+      );
+    });
+  }
 
   /**
    * Método para mostrar el contenido de una pestaña.
@@ -61,4 +70,10 @@ export class AccordionComponent implements OnInit {
       }
     }
   }
+
+  /**
+   * Método para asignar una nota a un usuario.
+   * @param form
+   **/
+  public onSubmit(form: NgForm): void {}
 }
